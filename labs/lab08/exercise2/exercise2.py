a7091eb631cf47d8e13e7428cbfb07020b08efca
alifaimanlawi08 <alifaimanlawi08@gmail.com>
2026-09-15 11:31:37 +0800

    lab 08

--- labs/lab08/exercise2/exercise2.py
+++ labs/lab08/exercise2/exercise2.py
@@ -3,6 +3,34 @@ base_salary = float(input())
 overtime_hours = int(input())
 tax_status = input()
 
+#calculate overtime pay and gross salary 
+overtime_pay = overtime_hours * 35
+gross_salary = base_salary + overtime_pay
+
+if tax_status == "Single":
+    if gross_salary >= 5000:
+        tax_rate = 0.22
+    else:
+        tax_rate = 0.18
+
+if tax_status == "Married":
+    if gross_salary >= 6000:
+        tax_rate = 0.20
+    else:
+        tax_rate = 0.15
+
+if tax_status == "Head":
+    if gross_salary >= 5500:
+        tax_rate = 0.25
+    else:
+        tax_rate = 0.19
+
+#calculate deductions
+income_tax = gross_salary * tax_rate
+EPF = gross_salary * 0.11
+SOCSO = gross_salary * 0.005
+
+net_salary = gross_salary - income_tax - EPF - SOCSO
 
 
 print(employee_name)
--- labs/lab08/exercise3/exercise3.py
+++ labs/lab08/exercise3/exercise3.py
@@ -1,8 +1,24 @@
 day_type = input()
 show_time = int(input())
 customer_type = input()
+if day_type == "weekday" and customer_type == "Child":
+    base_price = 10
+elif day_type == "weekday" and customer_type == "Adult":
+    base_price = 15
+else:
+    base_price = 12
 
+if day_type == "weekend" and customer_type == "Child":
+    base_price = 12
+elif day_type == "weekend" and customer_type == "Adult":
+    base_price = 18
+else:
+    base_price = 15   
 
+if show_time > 18:
+    final_price = base_price + 3
+else:
+    final_price = base_price
 
 print(base_price)
 print(final_price)
